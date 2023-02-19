a3be9819fbbd15044320fafd107c9c0326b9d0fe
joshuaramin <raminjoshua05@gmail.com>
2023-02-19 13:05:41 +0800

    updatesss

--- src/api/helpers/email.ts
+++ src/api/helpers/email.ts
@@ -10,29 +10,29 @@ import { google } from 'googleapis'
 **/
 
 const oAUth2Client = new google.auth.OAuth2({
-    clientId: process.env.GMAIL_CLIENTID, // ClientID 
-    clientSecret: process.env.GMAIL_CLIENTSECRET,
+    clientId: "782394693386-gooll1na41pkftagsb1np2d9scleeqfg.apps.googleusercontent.com", // ClientID 
+    clientSecret: "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",
     redirectUri: "https://developers.google.com/oauthplayground"
 })
 
 
-oAUth2Client.setCredentials({ refresh_token: process.env.GMAIL_REFRESHTOKEN })
+oAUth2Client.setCredentials({ refresh_token: "1//04FXnUOlIxqa7CgYIARAAGAQSNwF-L9Iro9-YzzVboDHkKEZ_5_QTPdfnSVOROlQ9dpBdOJcH4Skc_vFwM1um5nYpGhSaj_CEC4Y" })
 export async function GESend(Email: string, message: string, subject: string) {
     let accounts = nodemailer.createTransport({
         service: "gmail",
         auth: {
             type: "OAuth2",
-            user: process.env.GMAIL_ACCOUNT, //gmail account you useds to set the project up in google cloud console.
-            clientId: process.env.GMAIL_CLIENTID, // ClientID 
-            clientSecret: process.env.GMAIL_CLIENTSECRET, // Client Secret
-            refreshToken: process.env.GMAIL_REFRESHTOKEN, // Refresh Token
-            accessToken: process.env.ACCESSTOKEN // Access Token Variable we defined earlier
+            user: "dinotech26013@gmail.com", //gmail account you useds to set the project up in google cloud console.
+            clientId: "782394693386-gooll1na41pkftagsb1np2d9scleeqfg.apps.googleusercontent.com", // ClientID 
+            clientSecret: "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", // Client Secret
+            refreshToken: "1//04FXnUOlIxqa7CgYIARAAGAQSNwF-L9Iro9-YzzVboDHkKEZ_5_QTPdfnSVOROlQ9dpBdOJcH4Skc_vFwM1um5nYpGhSaj_CEC4Y", // Refresh Token
+            accessToken: "ya29.a0AVvZVsqC9ZrADOemh-V5dBo27O-2-0ohnjDtYhX2UIDfTHoNnKmh_fnibn4XLYAymrPQrazTfaPszQRCr0hOe_ipxdzAAqeKRLiAQbR0qLOVVlAyeZgI9lC7PlByFAwIiYXx9Za3XVixJ7XRCjAxzmvpKPTBaCgYKAdcSARMSFQGbdwaIj99uD19-4a3GlJihv82DCQ0163" // Access Token Variable we defined earlier
         }
     })
 
 
     await accounts.sendMail({
-        from: process.env.GMAIL_ACCOUNT,
+        from: "dinotech26013@gmail.com",
         to: Email,
         subject: subject,
         text: message,
